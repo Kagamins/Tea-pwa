3102341d575821c17dba946c2f0b99fbd28beefb
Kagamins <nsoulper@gmail.com>
2022-09-26 00:22:21 +0300

    minmax

---
+++ pages/announcement.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function Home() {
+  
+ 
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>  ثانويه عروه بن الزبير   </title>
+        <meta name="description" content="موقع مدرسه عروه بن الزبير" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
+
+    
+   
+
+      <main className={styles.main}>
+
+
+
+
+      </main>
+
+
+   </div>
+)
+}
+
