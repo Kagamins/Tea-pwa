862ea7dba229494e8f51d8669c1e0f6b47a77128
Kagamins <nsoulper@gmail.com>
2022-09-26 00:21:50 +0300

    additions

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,11 @@
 import { Navbar } from "reactstrap"
 
 export default function Footer (){
-    return (<Navbar color="dark" style={{borderTop: "3px solid #eaeaea"}} fixed="bottom">
+    return (<Navbar color="dark" style={{borderTop: "1px solid #eaeaea"}} container="fluid" fixed="bottom">
             <a className="btn btn-primary" href="./"> <i className=" fa-solid fa-house"> </i></a>
-            <a></a>
-        </Navbar>)
+            <a className="btn btn-danger" href="./announcement"> <i className=" fa-solid fa-bell"> </i></a>
+            <a className="btn btn-success" href="./"> <i className=" fa-solid fa-comment"> </i></a>
+            <a className="btn btn-warning" href="./calendar"> <i className=" fa-solid fa-calendar-days"> </i></a>
+            <a className="btn btn-secondary" href="./"> <i className=" fa-solid fa-circle-user"> </i></a>
+            </Navbar>)
 }
--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import { Navbar, NavbarBrand, NavbarText } from "reactstrap"
 import Image from "next/image"
 
 export default function Navibar (){
-    return (<Navbar fixed="top" container='fluid' color="white" navbar='true'>
+    return (<Navbar fixed="top" style={{borderBottom:"1px solid #141010"}} container='fluid' color="white" navbar='true'>
             <NavbarBrand >
               <Image alt="school-logo" src="/icon-384x384.png" height={60} width={60}></Image>  
             </NavbarBrand>
