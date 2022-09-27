0fcbf7ac7285789286dc53eec32077569959786a
Kagamins <nsoulper@gmail.com>
2022-09-27 10:02:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import { ListGroup,ListGroupItem } from 'reactstrap'
 
 export default function Home() {
