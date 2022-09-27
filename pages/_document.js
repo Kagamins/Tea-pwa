6bc94348006aa4629ccf7d30e5f79918312177db
Kagamins <nsoulper@gmail.com>
2022-09-27 10:09:53 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
     
-    <Script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-    crossorigin="anonymous"/>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
           <link rel="apple-touch-icon" href="/icon-384x384.png"></link>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"></link>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
           
         </Head>
         <body>
