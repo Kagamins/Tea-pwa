b92f5eac98dfb44c770c277a7a6d78074ca395ae
Kagamins <nsoulper@gmail.com>
2022-09-27 10:35:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
+const runtimeCaching = require('next-pwa/cache');
+
+const withPWA = require("next-pwa")({ dest: 'public',
+runtimeCaching,});
+
 /** @type {import('next').NextConfig} */
-
-const withPWA = require("next-pwa");
-const runtimeCaching = require('next-pwa/cache')
-
 
 
 module.exports = withPWA({
-    dest: 'public',
-    runtimeCaching,
+   
   
 });
 
